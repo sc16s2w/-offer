467d80c05c527bfce7a99fc6284b6fab29fb300c
sc16s2w <765012395@qq.com>
2021-05-19 20:52:36 +0200

    other

--- src/Heap/Code_51.java
+++ src/Heap/Code_51.java
@@ -2,13 +2,26 @@ package Heap;
 
 import java.util.Arrays;
 
+
+/**
+ * 在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。输入一个数组，求出这个数组中的逆序对的总数。
+ */
+//归并排序中实现逆序对
 public class Code_51 {
+    static int m = 0;
     public static void main(String[] args){
-        int[] merge = new int[]{2,4,1,3,7,9,8};
+        int[] merge = new int[]{4,5,6,7};
 //        int[] temp = merge(new int[]{1,5,7}, new int[]{2,3});
         int[] temp = sort(merge);
         for(int i:temp)
             System.out.println(i);
+        System.out.println(m);
+    }
+    public int reversePairs(int[] nums) {
+        int[] temp = sort(nums);
+        return m;
+
+
     }
     public static int[]  sort(int[] arr){
         if(arr.length<2)
@@ -19,7 +32,7 @@ public class Code_51 {
         return merge(sort(left),sort(right));
 
     }
-    public static int[] merge(int[] left, int[] right, int m){
+    public static int[] merge(int[] left, int[] right){
         int[] result = new int[left.length+right.length];
         int i = 0;
         int temp = 0;
@@ -31,7 +44,7 @@ public class Code_51 {
                 i++;
             }
             else{
-                m = m+1
+                m = m+left.length-i;
                 result[temp] = right[j];
                 temp++;
                 j++;
@@ -39,18 +52,13 @@ public class Code_51 {
 
         }
         while (i<left.length){
-            m = m+1;
             result[temp++]=left[i++];
         }
         while (j<right.length){
-            m = m+1
             result[temp++]=right[j++];
         }
-        return resultm;
+        return result;
     }
     //逆序对
     //如果不考虑时间复杂度，可以递归求解
-//    public int reversePairs(int[] nums) {
-//
-//    }
 }
